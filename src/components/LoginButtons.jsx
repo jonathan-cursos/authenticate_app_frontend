2a9bc6bfda6addaaf081ba825b88b43406998c88
Jonathan García <jona03g97@gmail.com>
2023-02-10 20:51:36 -0600

    Fix: Button href

--- src/components/LoginButtons.jsx
+++ src/components/LoginButtons.jsx
@@ -1,33 +1,24 @@
-import React from "react";
-import { FaGoogle, FaFacebookF, FaTwitter } from "react-icons/fa";
-import config from "../config";
+import React from 'react'
+import { FaGoogle, FaFacebookF, FaTwitter } from 'react-icons/fa'
+import config from '../config'
 
 const LoginButtons = () => {
   return (
     <>
-      <a
-        href={`${config.api}/auth/google`}
-        className="social__button google"
-      >
-        <FaGoogle className="btn__icon" />
+      <a href={`/auth/google`} className='social__button google'>
+        <FaGoogle className='btn__icon' />
         Enter with Google
       </a>
-      <a
-        href={`${config.api}/auth/facebook`}
-        className="social__button fb"
-      >
-        <FaFacebookF className="btn__icon" />
+      <a href={`/auth/facebook`} className='social__button fb'>
+        <FaFacebookF className='btn__icon' />
         Enter with Facebook
       </a>
-      <a
-        href={`${config.api}/auth/twitter`}
-        className="social__button tt"
-      >
-        <FaTwitter className="btn__icon" />
+      <a href={`/auth/twitter`} className='social__button tt'>
+        <FaTwitter className='btn__icon' />
         Enter with Twitter
       </a>
     </>
-  );
-};
+  )
+}
 
-export default LoginButtons;
+export default LoginButtons
